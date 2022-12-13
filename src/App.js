e9ef6f975fa39debe5854700b3c67b8d7dd0dd51
Benoit FRAINEUX <b.fraineux@2f2c.fr>
2022-12-13 19:03:32 +0100

    redux with OC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
-import './Styles/main.scss'
+import {Display} from "./Components/Display";
+import {PlayPauseButton} from "./Components/PlayPauseButton";
+import {PointScoredButton} from "./Components/PointScoredButton";
+import {ResetButton} from "./Components/ResetButton";
 
 function App() {
   return (
-    <>Empty Sheet</>
+    <div>
+      <Display />
+      <div className="buttons-row">
+        <PointScoredButton playerId="player1">Point Joueur 1</PointScoredButton>
+        <PointScoredButton playerId="player2">Point Joueur 2</PointScoredButton>
+      </div>
+      <div className="buttons-row">
+        <ResetButton />
+        <PlayPauseButton />
+      </div>
+    </div>
   )
 }
 
---
+++ src/Components/PlayPauseButton.js
@@ -0,0 +1,17 @@
+import {useDispatch} from "react-redux";
+import { playPause } from './../utils/store'
+
+export function PlayPauseButton() {
+  const dispatch = useDispatch()
+
+  return (
+    <button
+      className={"button"}
+      onClick={() => {
+        dispatch(playPause())
+      }}
+    >
+      Pause / Reprendre
+    </button>
+  )
+}
